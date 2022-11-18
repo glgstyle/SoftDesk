7ee6c36fd7ad3d7d24dda18095dfecd6d86fc35a
glgstyle <gleguenno@gmail.com>
2022-11-18 15:00:56 +0100

    Rename modules, create abstractUser in authenticate, modifications in settings

--- soft_desk_API/asgi.py
+++ soft_desk_API/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for IssueTrackingSystem project.
+ASGI config for soft_desk_API project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IssueTrackingSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soft_desk_API.settings')
 
 application = get_asgi_application()
